6b22199910fb56d5ff74b685aa9398229b85b50d
bozhidar.rusev <bojidar_rusev96@abv.bg>
2023-05-09 16:53:59 +0300

    Add app functionality

---
+++ src/constants/constants.ts
@@ -0,0 +1,9 @@
+export const SYMBOLS = [
+  { code: "FTSE:FSI", name: "FTSE 100" },
+  { code: "INX:IOM", name: "S&P 500" },
+  { code: "EURUSD", name: "Euro/Dollar" },
+  { code: "GBPUSD", name: "Pound/Dollar" },
+  { code: "IB.1:IEU", name: "Brent Crude Oil" },
+];
+export const API_URL =
+  "https://markets-data-api-proxy.ft.com/research/webservices/securities/v1/quotes";
